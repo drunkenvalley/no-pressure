347be8db8365bf97c390b4303671204bb8a8d80f
Iain Brookes <iain.brookes@pimberly.com>
2023-03-21 18:16:36 +0000

    finalised navbar styling, added nice deep background colour to match wireframe

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,23 @@ import { FetchUserCount, Discord } from "./components/body";
 
 function App() {
   return (
-    <div className="App font-nunito">
-      <Nav />
-      <FetchUserCount />
-      <div id="join-us">
-        <Discord />
-      </div>
+    <div className="App font-nunito h-full">
+      <header className="sticky top-0 pt-4 pb-[14px] z-10 bg-slate-800 border-solid border-b-2 border-yellow-400">
+        <Nav />
+      </header>
+      <main className="relative my-4">
+        <FetchUserCount />
+        <div className="h-96 scroll-mt-[88px]" id="join-us">
+          <Discord />
+        </div>
+        <div className="h-96 scroll-mt-[88px]" id="raid-progression">
+          <Discord />
+        </div>
+        <div className="h-96 scroll-mt-[88px]" id="corner">
+          <Discord />
+        </div>
+      </main>
+      <footer>Footer content here</footer>
     </div>
   );
 }
