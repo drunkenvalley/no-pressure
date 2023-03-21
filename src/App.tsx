16b0e9738449bb9fa9840aa0b50e8e062f914536
Jure Zajc <Jure.Zajc.95@Gmail.Com>
2023-03-21 13:53:16 +0100

    Added new component body , request to get number of users in the server and change the menu.

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,13 @@
 import "./App.css";
 import { Nav } from "./components/nav";
+import {FetchUserCount, Discord} from "./components/body";
 
 function App() {
   return (
     <div className="App">
       <Nav />
-      <div className="h-screen" id="home">
-        HOME
-      </div>
-      <div className="h-screen" id="join-us">
-        JOIN US
-      </div>
-      <div className="h-screen" id="raid-progression">
-        Raid progression
-      </div>
-      <div className="h-screen" id="corner">
-        Bruxy&apos;s Corner
-      </div>
+      <FetchUserCount />
+      <div id="join-us"><Discord /></div>
     </div>
   );
 }
