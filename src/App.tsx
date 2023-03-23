d19f81b892b9126f12a8691552a37d7b43b4cc0b
iainbrux <me@iainbrux.com>
2023-03-23 22:12:01 +0000

    finished navigation bar styling, moved around a couple of html elements just to tidy up App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,18 @@ import FeatureList from "./components/FeatureList";
 import HeroBanner from "./components/HeroBanner";
 import Nav from "./components/Nav";
 
-const App = () => (
-  <div className="App font-nunito h-full bg-purple text-gold">
-    <main className="max-w-5xl mx-auto flex flex-col gap-y-8 py-8">
-      <header className="sticky top-0 py-4 z-10">
+const App = () => {
+  return (
+    <div className="App font-nunito h-full bg-purple text-gold">
+      <main className="max-w-5xl mx-auto flex flex-col gap-y-8 py-8">
         <Nav />
-      </header>
-      <HeroBanner />
-      <FeatureList />
-      <Discord />
-      <footer>Footer content here</footer>
-    </main>
-  </div>
-);
+        <HeroBanner />
+        <FeatureList />
+        <Discord />
+        <footer>Footer content here</footer>
+      </main>
+    </div>
+  );
+};
 
 export default App;
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,6 +1,9 @@
+import { useEffect, useState } from "react";
 import logo from "@/assets/logo-ipsum-nav.svg";
 
 const Nav = () => {
+  const [navHasBackground, setNavHasBackground] = useState(false);
+
   const navItems = [
     { name: "Home", selector: "#home" },
     { name: "Join us", selector: "#join-us" },
@@ -15,25 +18,46 @@ const Nav = () => {
     }
   };
 
+  const applyBackground = () => {
+    const navDimensionsInPixels = 76;
+    if (window.scrollY >= navDimensionsInPixels) {
+      setNavHasBackground(true);
+    } else {
+      setNavHasBackground(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", applyBackground);
+    return () => window.removeEventListener("scroll", applyBackground);
+  }, []);
+
   return (
-    <nav className="flex w-full justify-around">
-      <img
-        className="cursor-pointer"
-        onClick={() => scrollToView("#home")}
-        src={logo}
-      />
-      <ul className="flex items-center">
-        {navItems.map(({ name, selector }) => (
-          <li
-            className="mx-4 cursor-pointer"
-            key={selector.replace("#", "")}
-            onClick={() => scrollToView(selector)}
-          >
-            {name}
-          </li>
-        ))}
-      </ul>
-    </nav>
+    <header
+      className={`sticky top-0 py-4 z-10 max-w-none w-[100vw] ml-[50%] translate-x-[-50%] ${
+        navHasBackground && "bg-purple/80 ease-in duration-300 backdrop-blur-sm"
+      }`}
+      id="navbar"
+    >
+      <nav className="flex justify-around" id="nav">
+        <img
+          className="cursor-pointer"
+          onClick={() => scrollToView("#home")}
+          src={logo}
+        />
+        <ul className="flex items-center">
+          {navItems.map(({ name, selector }) => (
+            <li
+              className="mx-4 cursor-pointer"
+              key={selector.replace("#", "")}
+              onClick={() => scrollToView(selector)}
+            >
+              {name}
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </header>
   );
 };
 
