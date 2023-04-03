534d8b113998ccb7979aeec1b39057ae738ee3ae
Rohit Gurav <110970889+realrohitgurav@users.noreply.github.com>
2023-04-03 17:03:39 +0530

    Changed color text to Green in RaiderCard.tsx

--- src/components/RaiderCard.tsx
+++ src/components/RaiderCard.tsx
@@ -15,7 +15,7 @@ const RaiderCard = ({
         <img className="h-16 w-16" src={raider.thumbnail_url} />
         <h5 className="text-sm mt-2.5">{raider.name}</h5>
         <p className="text-xs">{raid.summary}</p>
-        <p className="text-xs">
+        <p className="text-xs text-green">
           {raid.normal_bosses_killed} / {raid.heroic_bosses_killed} /{" "}
           {raid.mythic_bosses_killed}
         </p>
