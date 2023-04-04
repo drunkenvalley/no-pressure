e1ba80a360d5156db93ae66fc2f3ddc348430809
Drunkenvalley <drunkenvalley@gmail.com>
2023-04-04 23:20:33 +0200

    Updating Discord/community section

--- src/components/Discord.tsx
+++ src/components/Discord.tsx
@@ -10,44 +10,70 @@ const Discord = () => {
   const discordWidgetId = process.env.NEXT_PUBLIC_DISCORD_WIDGET_ID as string;
 
   return (
-    <div
-      className="flex flex-col lg:flex-row items-center justify-center center w-full gap-8 px-4 lg:px-0 my-4 scroll-mt-[136px]"
-      id="join-us"
-    >
-      <div className="w-full border border-blue rounded py-16 px-4 md:px-16 flex justify-center items-center gap-y-8 min-h-[500px]">
-        <div className="w-[400px] mx-auto">
-          <p className="text-yellow-400">
+    <div className="grid gap-8 md:grid-cols-2 p-4 md:p-0" id="join-us">
+      <div className="w-full h-full bg-gradient-to-b from-purple to-blue/25 flex flex-col gap-8 p-8 md:p-12 rounded-lg">
+        <article className="text-left text-yellow-400 flex flex-col gap-6">
+          <p>
             <strong className="font-bold text-center text-yellow-400">
               No Pressure
             </strong>{" "}
-            started as a European counterpart to{" "}
-            <Link href={wowMadeEasy}>WoW Made Easy</Link> - a community built on
-            enjoying Dragonflight together, with no pressure
+            began on reddit as a post with the opening pitch{" "}
+            <Link
+              className="text-green"
+              href="https://www.reddit.com/r/wow/comments/zqzmhx/now_for_eu_community_for_players_too_nervous_to/"
+            >
+              NOW FOR EU: Community for Players Too Nervous to Try Dungeons
+            </Link>
+            .
           </p>
-          <div className="my-4 p-8 border-blue border rounded-xl flex items-center justify-evenly gap-6">
-            <Image
-              alt="Frostwolf pup"
-              className="w-16 h-16 rounded-full"
-              height={64}
-              src="/frostwolfpup.jpg"
-              width={64}
-            />
-            <div className="flex flex-col align-center">
-              <h3 className="text-xl mb-4">No Pressure Community</h3>
-              <NextLink
-                className="hover:bg-green ease-in-out duration-300 bg-blue rounded p-3 join-us cursor-pointer"
-                href={discordInviteLink}
-                rel="noreferrer"
-                target="_blank"
-              >
-                Join us
-              </NextLink>
-            </div>
+          <p>
+            This is the foundation of the ethos of the community - to enable
+            players to enjoy{" "}
+            <Link
+              className="text-green"
+              href="https://dragonflight.blizzard.com/en-us/"
+            >
+              World of Warcraft: Dragonflight
+            </Link>{" "}
+            together, without pressure from others to perform. To relax and
+            enjoy the show together.
+          </p>
+          <p>
+            Join us today and find out what it&apos;s like to play with a no
+            pressure community. If you&apos;re in the US check out{" "}
+            <Link className="text-green" href={wowMadeEasy}>
+              WoW Made Easy
+            </Link>{" "}
+            - the original that inspired No Pressure.
+          </p>
+        </article>
+        <NextLink
+          className={`
+            p-8 drop-shadow-lg rounded-lg gap-6
+            bg-gradient-to-b from-dark via-dark to-blue/25 bg-[length:100%_200%] bg-top hover:bg-center
+            border-b-4 border-transparent hover:border-gold focus:border-gold
+            flex flex-col items-center justify-evenly
+            transition-all duration-300
+          `}
+          href={discordInviteLink}
+          rel="noreferrer"
+          target="_blank"
+        >
+          <Image
+            alt="Frostwolf pup"
+            className="w-16 h-16 rounded-full"
+            height={64}
+            src="/frostwolfpup.jpg"
+            width={64}
+          />
+          <div className="flex flex-col align-center">
+            <h3 className="text-xl mb-4">No Pressure Community</h3>
+            <p className="text-green">Join us</p>
           </div>
-        </div>
+        </NextLink>
       </div>
       <iframe
-        className="w-full md:w-[500px] h-[500px] flex-shrink-0"
+        className="w-full h-full min-h-[500px]"
         sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"
         src={`https://discord.com/widget?id=${discordWidgetId}&theme=dark`}
       />
