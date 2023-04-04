a2ea8b1202f0acac35d3231521450abae9945a4c
Drunkenvalley <drunkenvalley@gmail.com>
2023-04-05 00:38:21 +0200

    Revamped text, modified classes for card

--- src/components/Discord.tsx
+++ src/components/Discord.tsx
@@ -11,32 +11,31 @@ const Discord = () => {
 
   return (
     <div className="grid gap-8 md:grid-cols-2 p-4 md:p-0" id="join-us">
-      <div className="w-full h-full bg-gradient-to-b from-purple to-blue/25 flex flex-col gap-8 p-8 md:p-12 rounded-lg">
+      <div className="w-full h-full bg-gradient-to-b from-purple via-purple to-blue/25 bg-[length:150%_100%] bg-center flex flex-col gap-8 p-8 md:p-12 rounded-lg">
         <article className="text-left text-yellow-400 flex flex-col gap-6">
           <p>
             <strong className="font-bold text-center text-yellow-400">
               No Pressure
             </strong>{" "}
-            began on reddit as a post with the opening pitch{" "}
+            started with a Reddit post that read:{" "}
             <Link
               className="text-green"
               href="https://www.reddit.com/r/wow/comments/zqzmhx/now_for_eu_community_for_players_too_nervous_to/"
             >
               NOW FOR EU: Community for Players Too Nervous to Try Dungeons
             </Link>
-            .
           </p>
           <p>
-            This is the foundation of the ethos of the community - to enable
-            players to enjoy{" "}
+            This is the basis for our community&apos;s ethos — to enable players
+            to enjoy{" "}
             <Link
               className="text-green"
               href="https://dragonflight.blizzard.com/en-us/"
             >
               World of Warcraft: Dragonflight
             </Link>{" "}
-            together, without pressure from others to perform. To relax and
-            enjoy the show together.
+            together without stress from players. To have fun and learn
+            together, and do contents of all challenges.
           </p>
           <p>
             Join us today and find out what it&apos;s like to play with a no
@@ -49,11 +48,12 @@ const Discord = () => {
         </article>
         <NextLink
           className={`
-            p-8 drop-shadow-lg rounded-lg gap-6
-            bg-gradient-to-b from-dark via-dark to-blue/25 bg-[length:100%_200%] bg-top hover:bg-center
-            border-b-4 border-transparent hover:border-gold focus:border-gold
-            flex flex-col items-center justify-evenly
-            transition-all duration-300
+          p-8 drop-shadow-lg rounded-lg gap-8
+          bg-gradient-to-b from-dark via-dark to-blue/25 bg-[length:100%_200%] bg-top hover:bg-center focus:bg-center
+          border-b-4 border-transparent hover:border-gold focus:border-gold
+          aspect-[16/9]
+          flex flex-col items-center justify-center
+          transition-all duration-300
           `}
           href={discordInviteLink}
           rel="noreferrer"
@@ -67,7 +67,7 @@ const Discord = () => {
             width={64}
           />
           <div className="flex flex-col align-center">
-            <h3 className="text-xl mb-4">No Pressure Community</h3>
+            <h3 className="text-xl mb-1">No Pressure Community</h3>
             <p className="text-green">Join us</p>
           </div>
         </NextLink>
