6d33935f51b58f16b607760e027be5761c46b126
Drunkenvalley <drunkenvalley@gmail.com>
2023-05-06 01:17:47 +0200

    Added default className to GridSection

--- src/components/GridSection.tsx
+++ src/components/GridSection.tsx
@@ -2,7 +2,7 @@ interface Props
   extends React.HTMLProps<HTMLDivElement>,
     React.PropsWithChildren {}
 
-export const GridSection = ({ children, className, ...rest }: Props) => {
+export const GridSection = ({ children, className = "", ...rest }: Props) => {
   return (
     <div
       className={`grid gap-8 md:grid-cols-2 p-4 md:p-0 ${className}`.trim()}
