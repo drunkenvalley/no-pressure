d4047153ef9aecc58ec8a42961f8ad03207676a6
drunkenvalley <drunkenvalley@gmail.com>
2025-10-21 10:26:28 +0200

    Start migrating theme to css file

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+const config = {
   content: [
     './src/**/*.{js,ts,jsx,tsx}'
   ],
@@ -26,3 +26,5 @@ module.exports = {
   },
   plugins: []
 }
+
+export default config
