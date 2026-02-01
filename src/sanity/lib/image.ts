5a524b15d2ffdeb7ff14603fc377557d8bb26c0e
Drunkenvalley <drunkenvalley@gmail.com>
2026-02-01 12:55:47 +0100

    Hopefully fixed resolution issue now

--- src/sanity/lib/image.ts
+++ src/sanity/lib/image.ts
@@ -1,6 +1,7 @@
-import createImageUrlBuilder from "@sanity/image-url";
-import { SanityImageSource } from "@sanity/image-url/lib/types/types";
-
+import {
+  createImageUrlBuilder,
+  type SanityImageSource,
+} from "@sanity/image-url";
 import { dataset, projectId } from "../env";
 
 // https://www.sanity.io/docs/image-url
